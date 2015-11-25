9bca8bfccd5f3ac02377ac6f560cdb8804d03cd2
Steven Marshall <steven@nevvix.com>
2015-11-24 17:13:51 -0800

    Major update: Isolated the library from index.php & smi.css. Created smi.cls.php. Created smil.json. Updated smi.json.

--- README.md
+++ README.md
@@ -1,22 +1,37 @@
-# [smi](http://nevvix.github.io/smi)
+# [smi : social media icons &amp; links](http://nevvix.com/smi)
 
-## social media icons &amp; links
+## Create a social media icon bar without iframes or spying analytics.
 
 The goal is for anyone to use this code to create a custom social media icon bar without the `iframe`s used by social media widgets.
 
 The SVG icon images are produced from Dan Leech's [Simple Icons](https://simpleicons.org/) project.
 
-We avoid the HTTP requests involved in loading each icon image.
-
-All the social media links will be added to `index.html`.
-
-### Why use it?
+## Why use it?
 
 * ["Those tiny Tweet, Like, +1 buttons you see on websites are actually brutally large elements to load for (mobile) devices."](http://zurb.com/article/883/small-painful-buttons-why-social-media-bu)
 * ["Services like AddThis and ShareThis will always spy on and tag your audience when you use their widgets"](http://ma.tt/2014/07/canvas-fingerprinting-addthis)
+* ["If your image is instead a set of dom nodes, it cuts that extra HTTP request out, making your website faster and more user friendly."](http://code.tutsplus.com/articles/why-arent-you-using-svg--net-25414)
 
-## TODO
+## Usage
 
-* Add all the social media links to `index.html`.
-* Make a social media icon bar example for each icon size in `index.html`.
+Insert `require_once '{where smi is located}/smi/smi.cls.php';` at the top of your PHP page.
 
+Insert `<link rel="stylesheet" href="smi.css">` in the `<head>` section of your HTML code.
+
+Use the `smil.json` template to create your own data set of links and functionality.
+
+Then embed PHP `smi()` calls in your page.
+
+`smi()`'s optional arguments can be in native PHP array or JSON string format.
+
+## Examples
+
+&lt;?= smi(["space"=>"3px"]) ?&gt;
+
+&lt;?= smi('{"px": "24", "space": "1rem"}') ?&gt;
+
+&lt;?= smi(["px"=>"32", "icons"=>["Facebook","Google+","Pinterest"], "space"=>"4px"]) ?&gt;
+
+&lt;?= smi('{"px": "64", "icons": ["Facebook","Google+","Pinterest"], "space": "10px"}') ?&gt;
+
+&lt;?= smi('{"all": true, "px": "64"}') ?&gt;
---
+++ smi.cls.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * Create <ul> list of social media icons.
+ *
+ * Usage examples:
+ *     echo smi();
+ *     echo smi(['px'=>"32"]);
+ *     echo smi(['icons'=>["Facebook","Twitter","Email"]]);
+ *     echo smi('{"px": "24", "icons": ["Facebook","Twitter","Email"]}');
+ * @param array $args
+ * @return string
+ */
+function smi($args=NULL) {
+    return SMI::html($args);
+}
+
+class SMI {
+
+    static protected $js_urls = [], $icons_data, $links_data;
+
+    static function html($args=NULL) {
+
+        // Merge args
+        $default_args = [
+            'all'       => FALSE,
+            'icons'     => NULL,
+            'px'        => "16",
+            'space'     => "0", // http://www.w3.org/TR/CSS2/box.html#value-def-margin-width
+            'templates' => [
+                'ul'     => '<ul class="{{ul_class}}">{{lis}}</ul>',
+                'li'     => '    <li style="background-color:{{color}}; border-color:{{color}}; margin-right:{{space}};">{{a}}</li>',
+                'a'      => '{{script}}<a{{attrs}} rel="nofollow">{{svg}}</a>',
+                'script' => '<script async src="{{js}}"></script>',
+            ],
+            'filenames' => [
+                'icons' => __DIR__."/smi.json",
+                'links' => __DIR__."/smil.json",
+            ],
+        ];
+        if (is_string($args)) $args = json_decode($args, TRUE);
+        extract(array_merge($default_args, (array)$args)); // $all, $icons, $px, $space, $templates, $filenames
+
+        // Parse & cache json data
+        if (!isset(self::$icons_data)) self::$icons_data = self::json_read($filenames['icons']);
+        if (!isset(self::$links_data)) self::$links_data = self::json_read($filenames['links']);
+
+        // Filter icons set
+        if (!isset($icons)) $icons = (bool)$all ? array_keys(self::$icons_data) : array_keys(self::$links_data);
+        $icons_set = self::array_keep(self::$icons_data, $icons);
+        $links_set = self::array_keep(self::$links_data, $icons);
+        $icons_array = array_merge_recursive($icons_set, $links_set);
+ 
+        // Build HTML snippet keeping the order in $icons
+        $lis = [];
+        foreach ($icons as $icon) {
+
+            $data = $icons_array[$icon]; // keys = 'title', 'class', 'color', 'svg', 'js', 'onclick', 'url', 'href'
+
+            // js & <script>
+            $data['script'] = NULL;
+            if (isset($data['js'])) {
+                // Cache js urls to load <script> only once on the page
+                if (!in_array($data['js'], self::$js_urls)) {
+                    self::$js_urls []= $data['js'];
+                    $data['script'] = self::tr($templates['script'], ['js'=>$data['js']]);
+                }
+            }
+
+            // href
+            if (isset($data['href'])) $data['href'] = self::encode_query($data['href']);
+
+            // url & onclick
+            if (isset($data['url'])) $data['onclick'] = self::tr($data['onclick'], ['url'=>self::encode_query($data['url'])]);
+
+            // attrs
+            $data['attrs'] = self::attrs(self::array_remove($data, ['color', 'svg', 'js', 'script', 'url']));
+
+            // <a>
+            $data['a'] = self::tr($templates['a'], $data);
+
+            // <li>
+            $lis []= self::tr($templates['li'], $data + compact('space'));
+        }
+
+        // <ul>
+        $replacements = [
+            'ul_class' => "smi-".$px,
+            'lis'      => "\n".join("\n", $lis)."\n",
+        ];
+        return self::tr($templates['ul'], $replacements)."\n";
+    }
+
+    static function json_read($filename) {
+        return json_decode(@file_get_contents($filename), TRUE);
+    }
+
+    static function array_keep($array, $keys) {
+        return array_intersect_key($array, array_flip($keys));
+    }
+
+    static function array_remove($array, $keys) {
+        return array_diff_key($array, array_flip($keys));
+    }
+
+    static function encode_query($url, $enc_type=PHP_QUERY_RFC3986) {
+        if (!($query_string = parse_url($url, PHP_URL_QUERY))) return $url;
+        list($path, $fragment) = explode($query_string, $url, 2);
+        parse_str($query_string, $query_array);
+        $query = http_build_query($query_array, "", "&", $enc_type);
+        return join([$path, $query, $fragment]);
+    }
+
+    static function attrs($attributes, $attrs=[]) {
+        foreach ((array)$attributes as $key => $value)
+            $attrs []= sprintf(' %s="%s"', $key, $value);
+        return join($attrs);
+    }
+
+    static function tr($string, $replacements=NULL, $replace_pairs=[]) {
+        foreach ((array)$replacements as $key => $value)
+            $replace_pairs['{{'.$key.'}}'] = $value;
+        return strtr($string, $replace_pairs);
+    }
+
+}
